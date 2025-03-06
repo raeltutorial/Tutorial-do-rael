264104f3b67604d584846f4fc2f98e4eb0d58eab
raeltutorial <israel.editor08@gmail.com>
2025-03-06 11:55:57 -0300

    update meu script.lua

--- meu script.lua
+++ meu script.lua
@@ -2,8 +2,20 @@ local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/jen
 
 local Window = OrionLib:MakeWindow({Name = "Mochila hub", HidePremium = false, SaveConfig = false, ConfigFolder = "OrionTest"})
 
-local Tab = Window:MakeTab({
-	Name = "Tab 1",
+local main = Window:MakeTab({
+	Name = "Main",
 	Icon = "rbxassetid://4483345998",
 	PremiumOnly = false
+})
+
+main:AddSection({
+	Name = "Verificar jogadores"
+})
+
+main:AddButton({
+	Name = "Mostrar número de jogador",
+	Callback = function()
+    local Players = game.Players
+    print(#Players:GetChildren())
+  end
 })
